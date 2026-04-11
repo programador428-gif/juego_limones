3098a94ac80f2815998809d7f59dbf632d64036e
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-11 11:56:15 -0500

    movimiento a la izquierda añadido con una funcion que actualiza y limpia el canvas

--- limones.js
+++ limones.js
@@ -1,8 +1,13 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
+
+let personajeX = canvas.width / 2;
+
 const ALTURASUELO = 40;
 const ALTURAPERSONAJE = 60;
 const ANCHOPERSONAJE = 40;
+
+// Funciones
 function dibujarSuelo() {
     ctx.fillStyle = "green";
     ctx.fillRect(0, canvas.height - ALTURASUELO, canvas.width, ALTURASUELO);
@@ -10,9 +15,24 @@ function dibujarSuelo() {
 
 function dibujarPersonaje() {
     ctx.fillStyle = "#fbc18c";
-    ctx.fillRect((canvas.width / 2) - (ANCHOPERSONAJE / 2), canvas.height - ALTURAPERSONAJE - ALTURASUELO, ANCHOPERSONAJE, ALTURAPERSONAJE);
+    ctx.fillRect((personajeX) - (ANCHOPERSONAJE / 2), canvas.height - ALTURAPERSONAJE - ALTURASUELO, ANCHOPERSONAJE, ALTURAPERSONAJE);
 }
 
+function borrarCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function moverIzquierda() {
+    personajeX -= 10;
+    actualizarJuego();
+}
+
+function actualizarJuego() {
+    borrarCanvas();
+    dibujarPersonaje();
+    dibujarSuelo();
+}
+// Funcion para empezar el juego
 function iniciarJuego() {
     dibujarSuelo();
     dibujarPersonaje();
