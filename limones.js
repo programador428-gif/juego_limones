0e5eb77ff39f8a139a1fa63da947a9a47f982ed0
Dilan Solorzano <230013094+programador428-gif@users.noreply.github.com>
2026-04-11 11:35:03 -0500

    agregar funcion de dibujar elementos

--- limones.js
+++ limones.js
@@ -1,2 +1,19 @@
 let canvas = document.getElementById("areaJuego");
 let ctx = canvas.getContext("2d");
+const ALTURASUELO = 40;
+const ALTURAPERSONAJE = 60;
+const ANCHOPERSONAJE = 40;
+function dibujarSuelo() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, canvas.height - ALTURASUELO, canvas.width, ALTURASUELO);
+}
+
+function dibujarPersonaje() {
+    ctx.fillStyle = "#fbc18c";
+    ctx.fillRect((canvas.width / 2) - (ANCHOPERSONAJE / 2), canvas.height - ALTURAPERSONAJE - ALTURASUELO, ANCHOPERSONAJE, ALTURAPERSONAJE);
+}
+
+function iniciarJuego() {
+    dibujarSuelo();
+    dibujarPersonaje();
+}
